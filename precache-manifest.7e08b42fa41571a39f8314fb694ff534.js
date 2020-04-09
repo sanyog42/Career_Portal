a7f1c4bd6129d9c130cfe94587b9c93a854faace
Sanyog Gupta <sanyoggupta42@gmail.com>
2020-04-10 02:38:23 +0530

    deploy

---
+++ precache-manifest.7e08b42fa41571a39f8314fb694ff534.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "38ff600aee8c9d2e3187",
+    "url": "/Career_Portal/css/app.1edd81b1.css"
+  },
+  {
+    "revision": "38ff600aee8c9d2e3187",
+    "url": "/Career_Portal/js/app.a817e78e.js"
+  },
+  {
+    "revision": "5d468cc1b1b797c6daf5",
+    "url": "/Career_Portal/js/chunk-vendors.4bb4fbda.js"
+  },
+  {
+    "revision": "c0e1c3890353f05f70c4",
+    "url": "/Career_Portal/js/common.d89d8aed.js"
+  },
+  {
+    "revision": "ed2893a8d9c204c0ada0",
+    "url": "/Career_Portal/js/dashboard.efe79042.js"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/Career_Portal/fonts/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/Career_Portal/fonts/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/Career_Portal/fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/Career_Portal/fonts/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "258afc3fe195d30fe194a110b7a86e1d",
+    "url": "/Career_Portal/index.html"
+  },
+  {
+    "revision": "3b91359dcf1cf96c8440a7532ac61dda",
+    "url": "/Career_Portal/img/anime3.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Career_Portal/robots.txt"
+  }
+];
