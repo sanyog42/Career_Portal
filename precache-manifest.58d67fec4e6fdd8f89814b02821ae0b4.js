06e2d933c55e5c4b28e041cf05fd4cce6552c8e4
sanyog42 <sanyoggupta42@gmail.com>
2020-04-10 23:47:10 +0530

    Added Search function

--- precache-manifest.58d67fec4e6fdd8f89814b02821ae0b4.js
+++ precache-manifest.58d67fec4e6fdd8f89814b02821ae0b4.js
@@ -1,47 +1,55 @@
 self.__precacheManifest = [
   {
-    "revision": "4b7ce45ce1618af7c462",
+    "revision": "d750f74a90dc90beefcc",
     "url": "/Career_Portal/css/app.09a1eee2.css"
   },
   {
-    "revision": "4b7ce45ce1618af7c462",
-    "url": "/Career_Portal/js/app.b2e87400.js"
+    "revision": "d750f74a90dc90beefcc",
+    "url": "/Career_Portal/js/app.26d887cc.js"
   },
   {
-    "revision": "dbb02bfb2368249d78d7",
-    "url": "/Career_Portal/js/chunk-vendors.d62fc548.js"
+    "revision": "50c0035306530afcaa77",
+    "url": "/Career_Portal/js/chunk-vendors.737096b8.js"
   },
   {
-    "revision": "7982b31b191cf5357498",
-    "url": "/Career_Portal/js/common.44904acb.js"
+    "revision": "7d16e8961b66765370f0",
+    "url": "/Career_Portal/js/common.38b41943.js"
   },
   {
-    "revision": "31ae8eb30927f3e47126",
-    "url": "/Career_Portal/js/dashboard.54094caa.js"
+    "revision": "0f8a376a1b65b777a8a5",
+    "url": "/Career_Portal/js/dashboard.a4f1365d.js"
   },
   {
     "revision": "03ef1918e505c3e3471f9369ef7a638f",
     "url": "/Career_Portal/fonts/nucleo.03ef1918.eot"
   },
   {
-    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
-    "url": "/Career_Portal/fonts/nucleo.5987dd12.woff2"
-  },
-  {
     "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
     "url": "/Career_Portal/fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/Career_Portal/fonts/nucleo.5987dd12.woff2"
   },
   {
     "revision": "b17a118e13e53558658b681a0ebdad82",
     "url": "/Career_Portal/fonts/nucleo.b17a118e.ttf"
   },
   {
-    "revision": "aa20ab325f4eaf612fa810986a70b8fd",
+    "revision": "b2731e2c4351969f228d54347542cb75",
     "url": "/Career_Portal/index.html"
+  },
+  {
+    "revision": "fb2352b59ec879a73a262098068f1e5e",
+    "url": "/Career_Portal/apple-icon.png"
   },
   {
     "revision": "3b91359dcf1cf96c8440a7532ac61dda",
     "url": "/Career_Portal/img/anime3.png"
+  },
+  {
+    "revision": "fb2352b59ec879a73a262098068f1e5e",
+    "url": "/Career_Portal/img/apple-icon.png"
   },
   {
     "revision": "77acae85daaf054c5f52fb1c86d461fc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Career_Portal/precache-manifest.94c78a3ba3c6b0e036f5ed264b120638.js"
+  "/Career_Portal/precache-manifest.58d67fec4e6fdd8f89814b02821ae0b4.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "Career_Portal"});
